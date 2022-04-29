a7fc08a8633627296eab05ffdd535f11e1180cab
Nikto8962 <Brianmuiruri15@gmail.com>
2022-04-29 13:11:14 +0300

    more changes

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,41 @@
 import React from "react";
+import {
+  Cart,
+  Footer,
+  FooterBanner,
+  HeroBanner,
+  Layout,
+  Navbar,
+  Product,
+} from "../components";
+import { client } from "../lib/client";
 
-const index = () => {
+const index = ({ products, bannerData }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      Footer
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   );
 };
 
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 export default index;
